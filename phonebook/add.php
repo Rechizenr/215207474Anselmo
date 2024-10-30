09b8dd3d49096b5be2e16ef2a80b51279b4b5ed1
Belmar Anselmo <echizenr96@gmail.com>
2024-10-30 18:23:33 +0800

    sss

---
+++ phonebook/add.php
@@ -0,0 +1,43 @@
+<?php
+include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    if (!empty($name)&& !empty($email)) {
+        $sql= "INSERT INTO students (name, email) VALUES ('$name', '$email')";
+
+        if ($conn->query($sql)===TRUE){
+            
+        }
+            echo "New student added successfully!";
+
+        }
+        else{
+            echo "Failed to add a new student.";
+        }
+
+    }
+    else{
+        echo "Please fill in all fields";
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add New Student</title>
+</head>
+<body>
+    <h2>Add a New Student</h2>
+
+    <form method = "post" action="add.php">
+        Name: <input type="text" name="name"><br><br>
+        Email: <input type="text" email="email"><br><br>
+        <input type="submit" value="Add tudent">
+    </form>
+    <a href="index.php">Back to Student List</a>
+</body>
+</html>
