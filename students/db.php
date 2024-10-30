93ca3ecb4109f784765e0d4327ae070e90ba8da7
Belmar Anselmo <echizenr96@gmail.com>
2024-10-30 19:01:14 +0800

    ss

--- students/db.php
+++ students/db.php
@@ -2,10 +2,11 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "student_db";
+$dbname = "student_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error){
     die ("Connection failed". $conn->connect_error);
 }
+?>
